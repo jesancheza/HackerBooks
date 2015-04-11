d43e417c89716cb32eae1cf4f96ad610656fd3a8
José Enrique Sanchez <jesadt69@gmail.com>
2015-04-11 12:33:55 +0200

    Initial Commit

---
+++ HackerBooks2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerBooks2
+//
+//  Created by José Enrique Sanchez on 11/4/15.
+//  Copyright (c) 2015 Devappify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
